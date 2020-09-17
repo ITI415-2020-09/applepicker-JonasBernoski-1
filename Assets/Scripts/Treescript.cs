3aba24c25a5ef71fd73268f2cd15b05acdbf3798
Jonas Bernoski <jonas2015@aol.com>
2020-09-17 19:14:51 -0400

    Dropping Apples

--- Assets/Scripts/Treescript.cs
+++ Assets/Scripts/Treescript.cs
@@ -18,14 +18,35 @@ public class Treescript : MonoBehaviour
 
     void Start()
     {
-        
+        Invoke("DropApple", 2f);
     }
 
+    void DropApple()
+    {
+        GameObject Apple = Instantiate<GameObject>(ApplePrefab);
+        Apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
+
+
+        if (pos.x < -leftAndRightEdge) {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge) {
+            speed = Mathf.Abs(speed);
+        }
+    }
+    void FixedUpdate(){
+
+        if (Random.value < chanceToChangeDirections){
+            speed *= 1;
+
+        }
     }
 }
+
