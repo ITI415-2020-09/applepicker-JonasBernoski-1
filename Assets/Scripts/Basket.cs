c8e296d664adb4c6d870bf1a5fb7d1ee94559e2b
Jonas Bernoski <jonas2015@aol.com>
2020-09-17 20:57:49 -0400

    Score

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Basket : MonoBehaviour
-{
- 
+public class Basket : MonoBehaviour{
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
+    }
+
     void Update()
     {
         Vector3 mousePos2D = Input.mousePosition;
@@ -20,6 +29,10 @@ public class Basket : MonoBehaviour
         if(collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
         }
     }
 }
